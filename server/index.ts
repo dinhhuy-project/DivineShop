70c6705f1ad539359e47a8cbc73385adaa79fa4f
buidinhhuy <buidinhhuy900@gmail.com>
2025-07-10 17:53:07 +0700

    change to deploy

--- server/index.ts
+++ server/index.ts
@@ -65,7 +65,7 @@ app.use((req, res, next) => {
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
-    await setupVite(app, server);
+    serveStatic(app);
   }
 
   // ALWAYS serve the app on port 8079
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,19 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
 import path, { dirname } from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default defineConfig({
-  resolve: {
-    alias: {
+export default defineConfig(({ mode }) => {
+    return {
+      resolve: {
+        alias: {
       "@": path.resolve(__dirname, "client", "src"),
       "@shared": path.resolve(__dirname, "shared"),
       "@assets": path.resolve(__dirname, "attached_assets"),
-    },
-  },
-  root: path.resolve(__dirname, "client"),
-  build: {
-    outDir: path.resolve(__dirname, "dist/public"),
-    emptyOutDir: true,
-  },
+        }
+      },
+      root: path.resolve(__dirname, "client")
+    };
 });

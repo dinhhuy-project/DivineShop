723f5396b92b9fc1283a2d322871f3d878c14427
buidinhhuy <buidinhhuy900@gmail.com>
2025-07-10 17:39:36 +0700

    change to deploy

--- server/index.ts
+++ server/index.ts
@@ -65,7 +65,7 @@ app.use((req, res, next) => {
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
-    serveStatic(app);
+    await setupVite(app, server);
   }
 
   // ALWAYS serve the app on port 8079
